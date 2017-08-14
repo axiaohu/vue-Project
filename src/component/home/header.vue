eb0f7d0578db37af351d4d6409ad8385dd2e3a3b
XiaoHu <hi179308721@qq.com>
2017-08-14 15:08:27 +0800

    引入mintUI与MUI样式库

---
+++ src/component/home/header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <p>这是app的头部</p>
+    </header>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
+
--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,26 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
+import MintUI from 'mint-ui';       //MintUI是Vue插件，需要手动use才能使用
+import 'mint-ui/lib/style.css';    //引入样式库还需要单独导入CSS文件
+import 'mui/dist/css/mui.css';      //引入mui样式
+import 'mui/examples/hello-mui/css/icons-extra.css';  //引入mui的扩展图标样式
 
 // 1.手动use启用插件
 Vue.use(VueRouter);
 Vue.use(VueResource);
-
+Vue.use(MintUI);
 // 二、导入我们自己的东西
 
 // 2.1导入自己的组件
 import App from '../component/App.vue';   //根组件
 
-//2.2导入配置好的路由对象
-import router from './router.js'   //路由
+// 2.2导入配置好的路由对象
+import router from './router.js';   //路由
 
 // 三、创建一个Vue实例，关联视图，把根组件渲染到视图中
 new Vue({
     el: '#app',
     render: c => c(App),
-    router:router
+    router
 });
